2bdb66e34c17206144785d74fa773fe42dbbfda7
Konata9 <konata9@Konata9deMacBook-Pro.local>
2018-10-14 09:37:33 +0800

    完成 Chapter 3

---
+++ Chapter 3/factorial.js
@@ -0,0 +1,15 @@
+// 阶乘函数，最基本的递归
+
+function factorial(num) {
+  if (num === 1) {
+    // 基线条件，必须要 return 不然就不会出栈
+    return 1
+  } else {
+    // 递归条件，做了压栈操作
+    return num * factorial(num - 1)
+  }
+}
+
+console.log(factorial(3))
+console.log(factorial(4))
+console.log(factorial(5))
